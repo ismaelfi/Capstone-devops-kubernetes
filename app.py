2633c0ba3e4f4d2fe17fa6be464ea93c2383b594
FINNAOUI Ismail <ifinnaoui@gmail.com>
2021-09-19 23:49:55 +0200

    fix errors in app.py

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 def change(amount):
     # calculate the resultant change and store the result (res)
     res = []
-    coins 1,5,10,25] # value of pennies, nickels, dimes, quarters
+    coins = [1,5,10,25] # value of pennies, nickels, dimes, quarters
     coin_lookup = {25: "quarters", 10: "dimes", 5: "nickels", 1: "pennies"}
 
     # divide the amount*100 (the amount in cents) by a coin value
     # record the number of coins that evenly divide and the remainder
-    coin = coins.pop)
+    coin = coins.pop()
     num, rem  = divmod(int(amount*100), coin)
     # append the coin type and number of coins that had no remainder
     res.append({num:coin_lookup[coin]})
@@ -33,7 +33,7 @@ def hello():
 
 @app.route('/change/<dollar>/<cents>')
 def changeroute(dollar, cents):
-    prinf"Make Change for {dollar}.{cents}")
+    print(f"Make Change for {dollar}.{cents}")
     amount = f"{dollar}.{cents}"
     result = change(float(amount))
     return jsonify(result)
